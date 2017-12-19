5e3ac6b9a801d803703b1538ce1a80324bee7cff
Douglas Gusson <douglasgusson@gmail.com>
2017-12-19 17:14:51 -0200

    remove service worker

--- js/bingo.js
+++ js/bingo.js
@@ -98,10 +98,4 @@
 
     preencherArray();
 
-    if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
-    }
-
 })();
