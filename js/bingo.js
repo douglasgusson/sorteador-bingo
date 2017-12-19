31bf3d1bd42a55e010b9222937c937a183d0b414
Douglas Gusson <douglasgusson@gmail.com>
2017-12-19 16:43:37 -0200

    app reformulado

--- js/bingo.js
+++ js/bingo.js
@@ -1,79 +1,114 @@
-var sorteado, cont = 0, numeros = [];
+(function() {
+  'use strict';
 
-var $tds = document.querySelectorAll('.numero');
-var $jogadaAtual = document.querySelector('#jogada-atual');
-var $botaoSortear = document.querySelector('#sortear');
-var $playerAudio = document.querySelector('#audio');
-var $somMp3 = document.querySelector('#som-mp3');
-var $somOgg = document.querySelector('#som-ogg');
+/*
+    var sorteado, cont = 0, numeros = [];
 
-// preenche um array com numeros de 1 a 75
-function preencherArray() {
-    for (var i=1; i<=75; i++) {
-       numeros[i-1] = i;
-    }
-}
+    var $tds = document.querySelectorAll('.numero');
+    var $jogadaAtual = document.querySelector('#jogada-atual');
+    var $botaoRecomecar = document.querySelector('#recomecar');
+    var $botaoSortear = document.querySelector('#sortear');
+    var $playerAudio = document.querySelector('#audio');
+    var $somMp3 = document.querySelector('#som-mp3');
+    var $somOgg = document.querySelector('#som-ogg');
 
-$botaoSortear.addEventListener('click', function() {
-    if (cont < 75) {
-        sorteado = sortear();
+*/
 
-        if (sorteado <= 15) {
-            $jogadaAtual.setAttribute('value', 'Letra B - Número ' + sorteado);
-        } else if (sorteado <= 30) {
-            $jogadaAtual.setAttribute('value', 'Letra I - Número ' + sorteado);
-        } else if (sorteado <= 45) {
-            $jogadaAtual.setAttribute('value', 'Letra N - Número ' + sorteado);
-        } else if (sorteado <= 60) {
-            $jogadaAtual.setAttribute('value', 'Letra G - Número ' + sorteado);
-        } else {
-            $jogadaAtual.setAttribute('value', 'Letra O - Número ' + sorteado);
+    var app = {
+        container : document.querySelector('.main'),
+        carrega : document.querySelector('.carrega'),
+        sorteado : null, 
+        cont : 0,
+        numeros : [],
+        tds : document.querySelectorAll('.numero'),
+        jogadaAtual : document.querySelector('#jogada-atual'),
+        botaoRecomecar : document.querySelector('#recomecar'),
+        botaoSortear : document.querySelector('#sortear'),
+        playerAudio : document.querySelector('#audio'),
+        somMp3 : document.querySelector('#som-mp3'),
+        somOgg : document.querySelector('#som-ogg')
+    };
+
+    // preenche um array com numeros de 1 a 75
+    function preencherArray() {
+        for (var i=1; i<=75; i++) {
+           app.numeros[i-1] = i;
         }
+    }
 
-        marcarSorteado(sorteado);
-          
-    } else {
+    app.botaoRecomecar.addEventListener('click', function() {
         recomecar();
-    }
-});
+    });
 
+    app.botaoSortear.addEventListener('click', function() {
+        if (app.cont < 75) {
+            app.sorteado = sortear();
 
-function sortear() {
-    var index = Math.round(Math.random() * (74 - cont));
-    var x = numeros[index];
-    numeros.splice(index, 1);
-    cont++;
-    if (numeros.length == 0) {
-        $jogadaAtual.setAttribute('value', 'Encerrado!');
-        $botaoSortear.className += ' animated bounceIn botao-recomecar';
-        $botaoSortear.textContent = 'RECOMEÇAR';     
-    }
-    return x;
-}
+            if (app.sorteado <= 15) {
+                app.jogadaAtual.setAttribute('value', 'Letra B - Número ' + app.sorteado);
+            } else if (app.sorteado <= 30) {
+                app.jogadaAtual.setAttribute('value', 'Letra I - Número ' + app.sorteado);
+            } else if (app.sorteado <= 45) {
+                app.jogadaAtual.setAttribute('value', 'Letra N - Número ' + app.sorteado);
+            } else if (app.sorteado <= 60) {
+                app.jogadaAtual.setAttribute('value', 'Letra G - Número ' + app.sorteado);
+            } else {
+                app.jogadaAtual.setAttribute('value', 'Letra O - Número ' + app.sorteado);
+            }
 
-function recomecar() {    
-    if ($tds != null) {
-        for (var i = 0; i < $tds.length; i++) {
-                $tds[i].className = 'numero';
+            marcarSorteado(app.sorteado);
+              
+        } else {
+            recomecar();
         }
-    }    
-    $jogadaAtual.setAttribute('value', '');
-    $botaoSortear.className = 'botao botao-sortear';
-    $botaoSortear.textContent = 'SORTEAR';
-    cont = 0;
-    preencherArray();
-}
+    });
 
-function marcarSorteado(sorteado) {
-    if ($tds != null) {
-        for (var i = 0; i < $tds.length; i++) {
-            if (parseInt($tds[i].textContent) == sorteado) {
-                $tds[i].className += ' animated bounceIn sorteado';
-                $playerAudio.load();
-                $playerAudio.play();
+
+    function sortear() {
+        var index = Math.round(Math.random() * (74 - app.cont));
+        var x = app.numeros[index];
+        app.numeros.splice(index, 1);
+        app.cont++;
+        if (app.numeros.length == 0) {
+            app.jogadaAtual.setAttribute('value', 'Encerrado!');
+            app.botaoSortear.className += ' animated bounceIn orange lighten-1';
+            app.botaoSortear.textContent = 'RECOMEÇAR';     
+        }
+        return x;
+    }
+
+    function recomecar() {    
+        if (app.tds != null) {
+            for (var i = 0; i < app.tds.length; i++) {
+                    app.tds[i].className = 'numero';
+            }
+        }    
+        app.jogadaAtual.setAttribute('value', '');
+        app.botaoSortear.className = 'waves-effect waves-light btn-large btn-sortear';
+        app.botaoSortear.textContent = 'SORTEAR';
+        app.cont = 0;
+        preencherArray();
+    }
+
+    function marcarSorteado(sorteado) {
+        if (app.tds != null) {
+            for (var i = 0; i < app.tds.length; i++) {
+                if (parseInt(app.tds[i].textContent) == sorteado) {
+                    app.tds[i].className += ' animated bounceIn sorteado';
+                    app.playerAudio.load();
+                    app.playerAudio.play();
+                }
             }
         }
     }
-}
 
-preencherArray();
+
+    document.addEventListener("DOMContentLoaded", function(event) {
+        app.carrega.setAttribute('hidden', true);
+        app.container.removeAttribute('hidden');
+    });
+
+
+    preencherArray();
+
+})();
